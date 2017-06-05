b499368a898a27c4bf82f0a73b9cb9f6090a12b3
Sourcery Student <yiddo1993@gmail.com>
2017-06-04 23:52:58 -0700

    I have uploaded the file

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ int count;
+ for (count=10; count>0; count--)
+   {
+    cout << count << " ";
+   }
+ cout << " fire !!! ";
+
+ return 0;
+}
